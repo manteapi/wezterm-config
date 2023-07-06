96b899d484cf9367ee2a23dee6d17b52471a985d
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2023-07-06 09:10:18 +0200

    chore: little tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require("wezterm")
 return {
     window_close_confirmation = "NeverPrompt",
-    enable_wayland = false,
+    enable_wayland = true,
 	show_update_window = false,
 	check_for_updates = false,
 	enable_tab_bar = false,
 	color_scheme = "tokyonight_night",
 	font = wezterm.font("FiraCode Nerd Font"),
-	font_size = 14,
+	font_size = 16,
 	default_prog = { "/usr/bin/zsh" },
 }
