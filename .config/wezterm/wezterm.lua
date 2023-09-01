cb500f51b6ba018c150715594692f50c5e6869b2
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2023-09-01 20:42:44 +0200

    feat: set fontsize to 12

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,6 @@ return {
 	enable_tab_bar = false,
 	color_scheme = "tokyonight_night",
 	font = wezterm.font("FiraCode Nerd Font"),
-	font_size = 16,
+	font_size = 12,
 	default_prog = { "/usr/bin/zsh" },
 }
